0d29a8633d6eb11f868909e8bdfebf0ab723d977
Peter <107806301+Peeda@users.noreply.github.com>
2023-11-12 21:18:20 -0500

    reindented

--- configuration.nix
+++ configuration.nix
@@ -5,147 +5,147 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+    imports =
+        [ # Include the results of the hardware scan.
+        ./hardware-configuration.nix
+        ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+# Bootloader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    networking.hostName = "nixos"; # Define your hostname.
+# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+# Configure network proxy if necessary
+# networking.proxy.default = "http://user:password@proxy:port/";
+# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+# Enable networking
+        networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "America/New_York";
+# Set your time zone.
+    time.timeZone = "America/New_York";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+# Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-
-  programs.hyprland.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-    libinput = {
-        enable = true;
-	mouse = {
-	    accelProfile = "flat";
-	};
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "en_US.UTF-8";
+        LC_IDENTIFICATION = "en_US.UTF-8";
+        LC_MEASUREMENT = "en_US.UTF-8";
+        LC_MONETARY = "en_US.UTF-8";
+        LC_NAME = "en_US.UTF-8";
+        LC_NUMERIC = "en_US.UTF-8";
+        LC_PAPER = "en_US.UTF-8";
+        LC_TELEPHONE = "en_US.UTF-8";
+        LC_TIME = "en_US.UTF-8";
     };
-  };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+# Enable the X11 windowing system.
+    services.xserver.enable = true;
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+# Enable the KDE Plasma Desktop Environment.
+    services.xserver.displayManager.sddm.enable = true;
+    services.xserver.desktopManager.plasma5.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+    programs.hyprland.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+# Configure keymap in X11
+    services.xserver = {
+        layout = "us";
+        xkbVariant = "";
+        libinput = {
+            enable = true;
+            mouse = {
+                accelProfile = "flat";
+            };
+        };
+    };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  nixpkgs.config.allowUnfree = true;
-  users.users.peter = {
-    isNormalUser = true;
-    description = "peter";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-     gitFull
-     gh
+# Enable CUPS to print documents.
+    services.printing.enable = true;
+
+# Enable sound with pipewire.
+    sound.enable = true;
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+# If you want to use JACK applications, uncomment this
+#jack.enable = true;
+
+# use the example session manager (no others are packaged yet so this is enabled by default,
+# no need to redefine it in your config for now)
+#media-session.enable = true;
+    };
+
+# Enable touchpad support (enabled default in most desktopManager).
+# services.xserver.libinput.enable = true;
+
+# Define a user account. Don't forget to set a password with ‘passwd’.
+    nixpkgs.config.allowUnfree = true;
+    users.users.peter = {
+        isNormalUser = true;
+        description = "peter";
+        extraGroups = [ "networkmanager" "wheel" ];
+        packages = with pkgs; [
+            gitFull
+                gh
+        ];
+    };
+
+# List packages installed in system profile. To search, run:
+# $ nix search wget
+    environment.systemPackages = with pkgs; [
+        vim 
+            fish
+            openssh
     ];
-  };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  vim 
-  fish
-  openssh
-  ];
+    fonts.fonts = with pkgs; [
+        (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+            corefonts
+    ];
 
-  fonts.fonts = with pkgs; [
-    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-    corefonts
-  ];
+# Some programs need SUID wrappers, can be configured further or are
+# started in user sessions.
+# programs.mtr.enable = true;
+# programs.gnupg.agent = {
+#   enable = true;
+#   enableSSHSupport = true;
+# };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+# List services that you want to enable:
 
-  # List services that you want to enable:
+# Enable the OpenSSH daemon.
+    services.openssh.enable = true;
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+# Open ports in the firewall.
+# networking.firewall.allowedTCPPorts = [ ... ];
+# networking.firewall.allowedUDPPorts = [ ... ];
+# Or disable the firewall altogether.
+# networking.firewall.enable = false;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+# This value determines the NixOS release from which the default
+# settings for stateful data, like file locations and database versions
+# on your system were taken. It‘s perfectly fine and recommended to leave
+# this value at the release version of the first install of this system.
+# Before changing this value read the documentation for this option
+# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "23.05"; # Did you read the comment?
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+        nix.optimise.automatic = true;
 
-  nix.optimise.automatic = true;
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    environment.shells = with pkgs; [ zsh ];
+    users.defaultUserShell = pkgs.fish;
+    programs.fish.enable = true;
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  environment.shells = with pkgs; [ zsh ];
-  users.defaultUserShell = pkgs.fish;
-  programs.fish.enable = true;
-
-  systemd.extraConfig = ''
-      DefaultTimeoutStopSec=15s
-  '';
+    systemd.extraConfig = ''
+        DefaultTimeoutStopSec=15s
+        '';
 }
--- flake.nix
+++ flake.nix
@@ -1,27 +1,27 @@
 {
 
- description = "Main flake";
+    description = "Main flake";
 
- inputs = {
-  nixpkgs.url = "nixpkgs/nixos-23.05";
-  nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
-  home-manager.url = "github:nix-community/home-manager/release-23.05";
-  home-manager.inputs.nixpkgs.follows = "nixpkgs";
- };
+    inputs = {
+        nixpkgs.url = "nixpkgs/nixos-23.05";
+        nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
+        home-manager.url = "github:nix-community/home-manager/release-23.05";
+        home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    };
 
- outputs = { self, nixpkgs, home-manager, ...}:
-  let 
-   lib = nixpkgs.lib;
-   system = "x86_64-linux";
-   pkgs = nixpkgs.legacyPackages.${system};
-  in {
-  nixosConfigurations.nixos = lib.nixosSystem {
-    inherit system;
-    modules = [./configuration.nix];
-  };
-  homeConfigurations.peter = home-manager.lib.homeManagerConfiguration {
-    inherit pkgs;
-    modules = [./home.nix];
-   };
-  };
+    outputs = { self, nixpkgs, home-manager, ...}:
+        let 
+        lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    in {
+        nixosConfigurations.nixos = lib.nixosSystem {
+            inherit system;
+            modules = [./configuration.nix];
+        };
+        homeConfigurations.peter = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [./home.nix];
+        };
+    };
 }

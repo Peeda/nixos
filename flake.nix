d3cfbf716f51f84fa398fc8c9f289d4d30b5de45
Peter <107806301+Peeda@users.noreply.github.com>
2024-03-13 15:04:54 -0400

    move to surface directory

--- flake.nix
+++ flake.nix
@@ -17,10 +17,10 @@
     in
     {
     
-      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      nixosConfigurations.surface = nixpkgs.lib.nixosSystem {
           specialArgs = {inherit inputs;};
           modules = [ 
-            ./hosts/default/configuration.nix
+            ./hosts/surface/configuration.nix
             inputs.home-manager.nixosModules.default
           ];
         };
